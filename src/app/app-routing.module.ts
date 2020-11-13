ba921a4ea4386b02b035c1a96ec8c17830916884
hanszman <victor.hanszman@hotmail.com>
2020-11-12 23:58:27 -0300

    criação do componente de home e definição da estrutura do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Importando Componentes
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
 
-const routes: Routes = [];
+// Definindo Rotas
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
