d93f7d51a024d72d1d5671960b6e9b881ad75259
hanszman <victor.hanszman@hotmail.com>
2020-12-05 15:51:38 -0300

    redirecionamento para o login

--- src/app/componentes/login/auth.guard.ts
+++ src/app/componentes/login/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,15 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(!this.auth.usuarioLogado)
+      this.router.navigate(['/login']);
     return this.auth.usuarioLogado;
   }
   
