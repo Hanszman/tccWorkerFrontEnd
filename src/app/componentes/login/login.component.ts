b266493c22828d62e259baec54e94bbabf6ca568
hanszman <victor.hanszman@hotmail.com>
2020-12-09 20:30:47 -0300

    correção na validação

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -28,6 +28,14 @@ export class LoginComponent implements OnInit {
       msg_dsc_login.classList.add('invalid-feedback');
       msg_dsc_login.innerHTML = 'Informe seu usuário!';
     }
+    else {
+      var dsc_login = <HTMLElement>document.getElementById('dsc_login');
+      var msg_dsc_login = <HTMLElement>document.getElementById('msg_dsc_login');
+      dsc_login.classList.remove('is-invalid');
+      msg_dsc_login.classList.remove('invalid-feedback');
+      msg_dsc_login.innerHTML = '';
+    }
+
     if (this.usuario.dsc_senha == undefined || this.usuario.dsc_senha == '') {
       var dsc_senha = <HTMLElement>document.getElementById('dsc_senha');
       var msg_dsc_senha = <HTMLElement>document.getElementById('msg_dsc_senha');
@@ -35,20 +43,16 @@ export class LoginComponent implements OnInit {
       msg_dsc_senha.classList.add('invalid-feedback');
       msg_dsc_senha.innerHTML = 'Informe sua senha!';
     }
-    if (this.usuario.dsc_login !== undefined && this.usuario.dsc_login !== '' &&
-        this.usuario.dsc_senha !== undefined && this.usuario.dsc_senha !== ''){
-      var dsc_login = <HTMLElement>document.getElementById('dsc_login');
-      var msg_dsc_login = <HTMLElement>document.getElementById('msg_dsc_login');
-      dsc_login.classList.remove('is-invalid');
-      msg_dsc_login.classList.remove('invalid-feedback');
-      msg_dsc_login.innerHTML = '';
-
+    else {
       var dsc_senha = <HTMLElement>document.getElementById('dsc_senha');
       var msg_dsc_senha = <HTMLElement>document.getElementById('msg_dsc_senha');
       dsc_senha.classList.remove('is-invalid');
       msg_dsc_senha.classList.remove('invalid-feedback');
       msg_dsc_senha.innerHTML = '';
+    }
 
+    if (this.usuario.dsc_login !== undefined && this.usuario.dsc_login !== '' &&
+        this.usuario.dsc_senha !== undefined && this.usuario.dsc_senha !== ''){
       this.authService.fazerAuth(this.usuario);
     }
   }
