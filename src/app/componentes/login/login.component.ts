f54df42073ac30b99bae7022c71c77cf752ba867
hanszman <victor.hanszman@hotmail.com>
2020-12-09 20:25:28 -0300

    correção na validação de login utilizando bootstrap invalid

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -21,11 +21,35 @@ export class LoginComponent implements OnInit {
   }
 
   fazerLogin(){
-    if (this.usuario.dsc_login == undefined || this.usuario.dsc_login == '')
-      alert('Informe seu usuário!')
-    else if (this.usuario.dsc_senha == undefined || this.usuario.dsc_senha == '')
-      alert('Informe sua senha!')
-    else
+    if (this.usuario.dsc_login == undefined || this.usuario.dsc_login == '') {
+      var dsc_login = <HTMLElement>document.getElementById('dsc_login');
+      var msg_dsc_login = <HTMLElement>document.getElementById('msg_dsc_login');
+      dsc_login.classList.add('is-invalid');
+      msg_dsc_login.classList.add('invalid-feedback');
+      msg_dsc_login.innerHTML = 'Informe seu usuário!';
+    }
+    if (this.usuario.dsc_senha == undefined || this.usuario.dsc_senha == '') {
+      var dsc_senha = <HTMLElement>document.getElementById('dsc_senha');
+      var msg_dsc_senha = <HTMLElement>document.getElementById('msg_dsc_senha');
+      dsc_senha.classList.add('is-invalid');
+      msg_dsc_senha.classList.add('invalid-feedback');
+      msg_dsc_senha.innerHTML = 'Informe sua senha!';
+    }
+    if (this.usuario.dsc_login !== undefined && this.usuario.dsc_login !== '' &&
+        this.usuario.dsc_senha !== undefined && this.usuario.dsc_senha !== ''){
+      var dsc_login = <HTMLElement>document.getElementById('dsc_login');
+      var msg_dsc_login = <HTMLElement>document.getElementById('msg_dsc_login');
+      dsc_login.classList.remove('is-invalid');
+      msg_dsc_login.classList.remove('invalid-feedback');
+      msg_dsc_login.innerHTML = '';
+
+      var dsc_senha = <HTMLElement>document.getElementById('dsc_senha');
+      var msg_dsc_senha = <HTMLElement>document.getElementById('msg_dsc_senha');
+      dsc_senha.classList.remove('is-invalid');
+      msg_dsc_senha.classList.remove('invalid-feedback');
+      msg_dsc_senha.innerHTML = '';
+
       this.authService.fazerAuth(this.usuario);
+    }
   }
 }
