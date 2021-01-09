412dbbf89b54ccce93268046740b3626fe0b708e
hanszman <victor.hanszman@hotmail.com>
2021-01-09 15:39:34 -0300

    ajustes

--- src/app/componentes/geral/detail/detail.component.ts
+++ src/app/componentes/geral/detail/detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DetailService } from './detail.service';
 import { TranslateService } from '@ngx-translate/core';
 
