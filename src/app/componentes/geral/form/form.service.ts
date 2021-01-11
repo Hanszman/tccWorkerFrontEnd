d56e462115a9d3ee597780617584f5f4cf9e0861
hanszman <victor.hanszman@hotmail.com>
2021-01-11 20:25:15 -0300

    form service

--- src/app/componentes/geral/form/form.service.ts
+++ src/app/componentes/geral/form/form.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
 
-  constructor() { }
+  private apiURL = environment.apiURL;
+
+  constructor(private http: HttpClient) { }
+
+  postUpload(files: Set<File>) {
+    const formData = new FormData();
+    files.forEach(file => formData.append('file', file, file.name));
+    return this.http.post(this.apiURL + 'upload', formData);
+  }
+
+  postForm(url, data) {
+    console.log(this.apiURL + url);
+    console.log(data);
+    return this.http.post(this.apiURL + url, data, {observe: 'response'});
+  }
 }
