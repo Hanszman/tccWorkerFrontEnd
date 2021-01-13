4b9e09b9c75b8900a0b5c7c6b47058affbab9e34
hanszman <victor.hanszman@hotmail.com>
2021-01-13 17:50:14 -0300

    ajustes no form

--- src/app/componentes/geral/form/form.component.ts
+++ src/app/componentes/geral/form/form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { FormService } from './form.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-form',
@@ -12,17 +14,38 @@ export class FormComponent implements OnInit {
   @Input() config;
   @Input() url: string;
   @Input() id;
-  objetoFormGroup: any
+  objetoFormGroup: any;
+  registro: any = {};
+  traducoes;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private service: FormService,
+    private translate: TranslateService
   ) { }
 
   ngOnInit(): void {
     this.objetoFormGroup = this.formBuilder.group({});
+    this.traduzir().subscribe((traducoes) => {
+      this.traducoes = traducoes;
+    })
   }
 
   enviar(){
 
+  }
+
+  traduzir(){
+    let idioma = 'br';
+    this.translate.use(idioma);
+    return this.translate.get(this.config.titulo);
+  }
+
+  tradutor(chave){
+    if (this.traducoes !== undefined){
+      if (chave in this.traducoes)
+        return this.traducoes[chave];
+      return '';
+    }
   }
 }
