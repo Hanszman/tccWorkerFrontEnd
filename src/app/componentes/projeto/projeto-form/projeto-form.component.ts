faf7e817a44d5cfa04accb811da344ec945b5693
hanszman <victor.hanszman@hotmail.com>
2021-01-25 20:35:08 -0300

    projetos form

--- src/app/componentes/projeto/projeto-form/projeto-form.component.ts
+++ src/app/componentes/projeto/projeto-form/projeto-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-projeto-form',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjetoFormComponent implements OnInit {
 
-  constructor() { }
+  id;
+  url = 'projeto';
+  titulo = 'Projeto';
+  operacao = 'Cadastrar';
+  mensagem = 'Cadastre um novo projeto';
+  @Input() config = {
+    titulo: 'projeto',
+    cabecalhos: [
+      'dsc_nome',
+      'dsc_descricao',
+      'dat_inicio',
+      'dat_fim',
+      'id_setor'
+    ],
+    tipos: [
+      'text',
+      'text',
+      'date',
+      'date',
+      'select'
+    ],
+    selects: {
+      id_setor: {
+        values: [],
+        labels: []
+      }
+    },
+    mascaras: [],
+    obrigatorios: [
+      'dsc_nome'
+    ],
+    desabilitados: []
+  };
+
+  constructor(
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe(params => this.id = params['id']);
+    if(this.id !== undefined) {
+      this.operacao = 'Editar';
+      this.mensagem = 'Edite o projeto selecionado';
+    }
+  }
 
   ngOnInit(): void {
   }
-
 }
