fbbb6ddc33064a923921b78a0af8e64abcdf0692
hanszman <victor.hanszman@hotmail.com>
2021-01-25 19:50:37 -0300

    table projeto

--- src/app/componentes/projeto/projeto-read/projeto-read.component.ts
+++ src/app/componentes/projeto/projeto-read/projeto-read.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projeto-read',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjetoReadComponent implements OnInit {
 
-  constructor() { }
+  url = 'projeto';
+  titulo = 'Projeto';
+  parametros;
+  id_empresa = window.localStorage.getItem('id_empresa');
+  @Input() config = {
+    titulo: 'projeto',
+    cabecalhos: [
+      'dsc_nome',
+      'dsc_descricao',
+      'dat_inicio',
+      'dat_fim',
+      'id_setor'
+    ],
+    paginacao: 5
+  };
+
+  constructor(
+    private router: Router
+  ) {
+    this.parametros = 'id_empresa=' + this.id_empresa + '&';
+  }
 
   ngOnInit(): void {
   }
-
 }
