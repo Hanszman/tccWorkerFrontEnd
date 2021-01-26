69eb4fabf3a208822cbf797114dc65e07502f477
hanszman <victor.hanszman@hotmail.com>
2021-01-25 21:32:22 -0300

    projeto detail

--- src/app/componentes/projeto/projeto-detail/projeto-detail.component.ts
+++ src/app/componentes/projeto/projeto-detail/projeto-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-projeto-detail',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjetoDetailComponent implements OnInit {
 
-  constructor() { }
+  id;
+  url = 'projeto';
+  titulo = 'Projeto';
+  parametros;
+  @Input() config = {
+    titulo: 'projeto',
+    cabecalhos: [
+      'dsc_nome',
+      'dsc_descricao',
+      'dat_inicio',
+      'dat_fim',
+      'dsc_setor'
+    ]
+  };
+
+  constructor(
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe(params => this.id = params['id']);
+    this.parametros = 'id_projeto=' + this.id + '&';
+  }
 
   ngOnInit(): void {
   }
-
 }
