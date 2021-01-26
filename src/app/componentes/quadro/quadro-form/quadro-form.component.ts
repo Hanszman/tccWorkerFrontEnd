57237402af8013e8869780f0bf3a4b44e99f272b
hanszman <victor.hanszman@hotmail.com>
2021-01-26 20:51:34 -0300

    form de quadros

--- src/app/componentes/quadro/quadro-form/quadro-form.component.ts
+++ src/app/componentes/quadro/quadro-form/quadro-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '../../geral/http/http.service';
 
 @Component({
   selector: 'app-quadro-form',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuadroFormComponent implements OnInit {
 
-  constructor() { }
+  id;
+  url = 'quadro';
+  titulo = 'Quadro';
+  operacao = 'Cadastrar';
+  mensagem = 'Cadastre um novo quadro';
+  parametros;
+  id_empresa = window.localStorage.getItem('id_empresa');
+  @Input() config = {
+    titulo: 'quadro',
+    cabecalhos: [
+      'dsc_nome',
+      'dsc_descricao',
+      'dat_inicio',
+      'dat_fim',
+      'id_projeto'
+    ],
+    tipos: [
+      'text',
+      'text',
+      'date',
+      'date',
+      'select'
+    ],
+    selects: {
+      id_projeto: {
+        values: [],
+        labels: []
+      }
+    },
+    mascaras: [],
+    obrigatorios: [
+      'dsc_nome',
+      'id_projeto'
+    ],
+    desabilitados: []
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: HttpService,
+  ) {
+    this.route.params.subscribe(params => this.id = params['id']);
+    if(this.id !== undefined) {
+      this.operacao = 'Editar';
+      this.mensagem = 'Edite o quadro selecionado';
+    }
+    this.parametros = 'id_empresa=' + this.id_empresa + '&';
+    this.service.getConsultar('projeto', this.parametros).subscribe((obj) => {
+      let conjunto = obj.body.data.dados;
+      for (let i = 0; i < conjunto.length; i++) {
+        this.config.selects.id_projeto.values.push(conjunto[i]['id_projeto'])
+        this.config.selects.id_projeto.labels.push(conjunto[i]['dsc_nome'])
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
-
 }
