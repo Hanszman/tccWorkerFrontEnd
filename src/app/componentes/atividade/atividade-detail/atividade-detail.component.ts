e59f1f7d45730234f0ef9b8ea4464feb41e6656f
hanszman <victor.hanszman@hotmail.com>
2021-02-06 16:36:16 -0300

    crud de atividade_usuario_empresa no front

--- src/app/componentes/atividade/atividade-detail/atividade-detail.component.ts
+++ src/app/componentes/atividade/atividade-detail/atividade-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpService } from '../../geral/http/http.service';
+import { TranslateService } from '@ngx-translate/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { ModalComponent } from '../../geral/modal/modal.component';
 
 @Component({
   selector: 'app-atividade-detail',
@@ -10,8 +14,14 @@ export class AtividadeDetailComponent implements OnInit {
 
   id;
   url = 'atividade';
+  urlFuncionario = 'atividade_usuario_empresa';
   titulo = 'Atividade';
+  tituloFuncionario = 'Funcionário';
+  relacoes;
   parametros;
+  parametrosConsulta;
+  traducoesFuncionario;
+  id_empresa = window.localStorage.getItem('id_empresa');
   @Input() config = {
     titulo: 'atividade',
     cabecalhos: [
@@ -24,14 +34,111 @@ export class AtividadeDetailComponent implements OnInit {
       'dsc_projeto'
     ]
   };
+  @Input() configFuncionario = {
+    titulo: 'atividade_usuario_empresa',
+    cabecalhos: [
+      'dsc_nome_completo_usuario_empresa',
+      'dsc_login_usuario_empresa',
+      'dsc_cargo_usuario_empresa'
+    ],
+    paginacao: 5
+  };
+  @Input() configAtividadeFuncionario = {
+    titulo: 'atividade_usuario_empresa',
+    cabecalhos: [
+      'id_usuario_empresa'
+    ],
+    tipos: [
+      'select'
+    ],
+    selects: {
+      id_usuario_empresa: {
+        values: [],
+        labels: []
+      }
+    },
+    mascaras: [],
+    obrigatorios: [
+      'id_usuario_empresa'
+    ],
+    desabilitados: []
+  }
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: HttpService,
+    private translate: TranslateService,
+    private modalService: BsModalService
   ) {
     this.route.params.subscribe(params => this.id = params['id']);
+    this.relacoes = {id_atividade: this.id};
     this.parametros = 'id_atividade=' + this.id + '&';
+    this.parametrosConsulta = 'id_empresa=' + this.id_empresa + '&';
+    this.service.getConsultar('usuario', this.parametrosConsulta).subscribe((obj) => {
+      let conjunto = obj.body.data.dados;
+      for (let i = 0; i < conjunto.length; i++) {
+        this.configAtividadeFuncionario.selects.id_usuario_empresa.values.push(conjunto[i]['id_usuario_empresa']);
+        this.configAtividadeFuncionario.selects.id_usuario_empresa.labels.push(conjunto[i]['dsc_nome_completo']);
+      }
+    });
   }
 
   ngOnInit(): void {
+    this.traduzir('funcionario').subscribe((traducoesFuncionario) => {
+      this.traducoesFuncionario = traducoesFuncionario;
+    })
+  }
+
+  emiteClicaBotaoCriarEspecialFuncionario(){
+    const initialState = {
+      config: this.configAtividadeFuncionario,
+      url: this.urlFuncionario,
+      dadosRelacao: this.relacoes,
+      traducoes: this.traducoesFuncionario
+    };
+    const modalRef = this.modalService.show(ModalComponent, {initialState});
+    modalRef.content.titulo = 'Vincular Funcionário à Atividade'
+    modalRef.content.existeModalForm = true;
+    modalRef.content.existeBotaoCriar = true;
+  }
+
+  emiteClicaBotaoDetalhesEspecialFuncionario(linha){
+    window.scrollTo(0,0);
+    this.router.navigate(['usuario/read/'], {queryParams: {id_funcionario_parametro: linha.id_usuario}});
+  }
+
+  emiteClicaBotaoExcluirEspecialFuncionario(linha){
+    const modalRef = this.modalService.show(ModalComponent);
+    modalRef.content.titulo = 'Desvincular Funcionário da Atividade';
+    modalRef.content.mensagem = 'Tem certeza que deseja desvincular esse funcionário da atividade?';
+    modalRef.content.existeMensagem = true;
+    modalRef.content.existeBotaoExcluir = true;
+    modalRef.content.emiteClicaBotaoExcluir.subscribe(() => {
+      this.service.deleteExcluir(this.urlFuncionario + '/delete', linha['id_' + this.urlFuncionario]).subscribe(resp => {
+        this.verificarResposta(resp);
+      });
+    });
+  }
+
+  verificarResposta(resp){
+    if (resp.body['data']['sucesso']){
+      alert(resp.body['data']['mensagem']);
+      window.location.reload();
+    }
+    else
+      alert(resp.body['data']['mensagem']);
+  }
+
+  traduzir(rotulo){
+    let idioma = 'br';
+    let tituloConfig;
+    this.translate.use(idioma);
+    switch (rotulo) {
+      case 'funcionario':
+        tituloConfig = this.configAtividadeFuncionario.titulo;
+        break;
+    }
+    return this.translate.get(tituloConfig);
   }
 }
