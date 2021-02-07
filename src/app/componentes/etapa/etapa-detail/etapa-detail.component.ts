d542821d7f7600796b8e30de47b5cb2f09cbee7a
hanszman <victor.hanszman@hotmail.com>
2021-02-07 16:17:45 -0300

    atividades vinculadas às etapas

--- src/app/componentes/etapa/etapa-detail/etapa-detail.component.ts
+++ src/app/componentes/etapa/etapa-detail/etapa-detail.component.ts
@@ -10,7 +10,9 @@ export class EtapaDetailComponent implements OnInit {
 
   id;
   url = 'etapa';
+  urlAtividade = 'atividade';
   titulo = 'Etapa';
+  tituloAtividade = 'Atividade';
   parametros;
   @Input() config = {
     titulo: 'etapa',
@@ -18,6 +20,16 @@ export class EtapaDetailComponent implements OnInit {
       'dsc_etapa',
       'ind_sequencia'
     ]
+  };
+  @Input() configAtividade = {
+    titulo: 'atividade',
+    cabecalhos: [
+      'dsc_nome',
+      'dsc_descricao',
+      'dat_inicio',
+      'dat_fim'
+    ],
+    paginacao: 5
   };
 
   constructor(
