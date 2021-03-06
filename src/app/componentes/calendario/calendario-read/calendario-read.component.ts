518158acc50650d50d527c5fd5ea2517705f3f00
hanszman <victor.hanszman@hotmail.com>
2021-03-06 17:06:41 -0300

    lista de etapas no calendário

--- src/app/componentes/calendario/calendario-read/calendario-read.component.ts
+++ src/app/componentes/calendario/calendario-read/calendario-read.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/angular';
 import ptBrLocale from '@fullcalendar/core/locales/pt-br';
+import { ChartComponent } from '../../geral/chart/chart.component';
+import { HttpService } from '../../geral/http/http.service';
 
 @Component({
   selector: 'app-calendario-read',
@@ -9,10 +11,13 @@ import ptBrLocale from '@fullcalendar/core/locales/pt-br';
 })
 export class CalendarioReadComponent implements OnInit {
 
+  listaEtapas = [];
+  listaAtividades = [];
   url = 'atividade';
   titulo = 'Atividade';
   parametros;
   id_empresa = window.localStorage.getItem('id_empresa');
+  private componenteChart = new ChartComponent();
   @Input() config = {
     titulo: 'atividade',
     cabecalhos: [
@@ -28,11 +33,25 @@ export class CalendarioReadComponent implements OnInit {
     paginacao: 5
   };
 
-  constructor() {
+  constructor(
+    private service: HttpService
+  ) {
     this.parametros = 'id_empresa=' + this.id_empresa + '&';
+    this.service.getConsultar('etapa', this.parametros).subscribe((obj) => {
+      let conjunto = obj.body.data.dados;
+      let cores = this.componenteChart.selecionaCores(conjunto.length);
+      for (let i = 0; i < conjunto.length; i++) {
+        this.listaEtapas[i] = new Object();
+        this.listaEtapas[i]['id_etapa'] = conjunto[i]['id_etapa'];
+        this.listaEtapas[i]['dsc_etapa'] = conjunto[i]['dsc_etapa'];
+        this.listaEtapas[i]['ind_sequencia'] = conjunto[i]['ind_sequencia'];
+        this.listaEtapas[i]['dsc_cor'] = cores[i];
+      }
+    });
   }
 
   ngOnInit(): void {
+    console.log(this.listaEtapas)
   }
 
   calendarOptions: CalendarOptions = {
